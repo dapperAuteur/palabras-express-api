458c75d209ffc209a4eb1d85e13469f205aad98d
Anthony McDonald <a@awews.com>
2018-01-26 16:27:22 -0500

    refactor to use port 8081

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+// require("dotenv").config()
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3030,
+    // cors = require('cors'),
     bodyParser = require('body-parser');
+    // authRoutes = require("./routes/auth");
 
+var db = require("./models");
 var palabraRoutes = require('./routes/palabras');
 var verboRoutes = require('./routes/verbos');
 var prefixSuffixRootsRoutes = require('./routes/prefixSuffixRoots');
 
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,10 +18,25 @@ app.get('/', function (req, res) {
   res.send("Hola From Root Route");
 });
 
-app.use('/api/palabras', palabraRoutes);
-app.use('/api/verbos', verboRoutes);
-app.use('/api/prefixSuffixRoots', prefixSuffixRootsRoutes);
+// app.use('/api/users/:id/games',
+//         auth.loginRequired, auth.ensureCorrectUser,
+//         gamesRoutes);
+// app.use('/api/auth', authRoutes);
+// app.get('/api/games', function (req, res, next) {
+//   .db.Game.find().sort({ createdAt: 'desc' })
+//     .populate("userId", { username: true, profileImageUrl: true })
+//     .then(function (games) {
+//       res.json(games);
+//     }).catch(function (err) {
+//       res.status(500).json(err);
+//     })
+// });
+app.use('/api/ver0001/palabras', palabraRoutes);
+app.use('/api/ver0001/verbos', verboRoutes);
+app.use('/api/ver0001/prefixSuffixRoots', prefixSuffixRootsRoutes);
 
-app.listen(port, function () {
-  console.log("My App is Running on port " + port);
+const PORT = 8081;
+
+app.listen(PORT, function () {
+  console.log(`My App is Running on port ${PORT}`);
 });
