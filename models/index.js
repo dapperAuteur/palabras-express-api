249c74326cd2aed7e7504e5555ff7c36b9708346
Anthony McDonald <a@awews.com>
2018-03-15 22:03:58 -0400

    add MONGODB_URI

--- models/index.js
+++ models/index.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/fourLetterWord-api', {
+// mongoose.connect('mongodb://localhost/fourLetterWord-api', {
+//   keepAlive: true,
+//   reconnectTries: Number.MAX_VALUE,
+//   useMongoClient: true
+// });
+mongoose.connect('MONGODB_URI', {
   keepAlive: true,
   reconnectTries: Number.MAX_VALUE,
   useMongoClient: true
