72effe68848f3b5c24fd7bc81bc05d76a83df539
Anthony McDonald <a@awews.com>
2018-03-16 07:37:28 -0400

    remove 1 endpoint

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ app.get('/', function (req, res) {
 app.use('/api/ver0001/users/:id/games',
         auth.loginRequired, auth.ensureCorrectUser,
         gameRoutes);
-app.use('/api/ver0001/users',
-        // auth.loginRequired,
-        userRoutes);
+// app.use('/api/ver0001/users',
+//         // auth.loginRequired,
+//         userRoutes);
 app.use('/api/ver0001/auth', authRoutes);
 // app.get('/api/ver0001/games', function (req, res, next) {
 //   db.Game.find().sort({ createdAt: 'desc' })
