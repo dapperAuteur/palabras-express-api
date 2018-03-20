363984d0fea85cd1091ed2122c480ef7ba957db3
Anthony McDonald <a@awews.com>
2018-03-19 20:19:11 -0400

    point to heroku database

--- models/index.js
+++ models/index.js
@@ -1,16 +1,16 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/fourLetterWord-api', {
-  keepAlive: true,
-  reconnectTries: Number.MAX_VALUE,
-  useMongoClient: true
-});
-// mongoose.connect(process.env.MONGODB_URI, {
+// mongoose.connect('mongodb://localhost/fourLetterWord-api', {
 //   keepAlive: true,
 //   reconnectTries: Number.MAX_VALUE,
 //   useMongoClient: true
 // });
+mongoose.connect(process.env.MONGODB_URI, {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE,
+  useMongoClient: true
+});
 
 module.exports.FourLetterWord = require("./fourLetterWord");
 module.exports.Verbo = require("./verbo");
