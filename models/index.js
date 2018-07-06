6f717700bca70d0b181a8b077338a394be7f5ae6
Anthony McDonald <a@awews.com>
2018-07-06 17:30:45 -0400

    add or statement to db connection to prevent future errors, now it checks for hosted db before trying local db

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/fourLetterWord-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fourLetterWord-api', {
   keepAlive: true,
   reconnectTries: Number.MAX_VALUE,
   useMongoClient: true
